6cefd3e07164dd54768ca6b7f1c511251c69f6c4
Garen Sahagian <g.s.sahagian@gmail.com>
2020-03-29 15:04:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(string)
   reverse_array = string.split(/ /)
   pp reverse_array
+  puts reverse_array[0].reverse
   reverse_array.each {|word| word.reverse}
   reverse_array
 end
