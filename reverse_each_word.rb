b7e749c314b1851224c973aff962f051d6069c25
Garen Sahagian <g.s.sahagian@gmail.com>
2020-03-29 15:58:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,6 +5,6 @@ def reverse_each_word(string)
   array.collect do |word| 
     reversed_words.concat("#{word.reverse}#{word==array.last ? "":" "}")
   end
-  #puts reversed_words
-  #reversed_words
+  puts reversed_words
+  reversed_words
 end
