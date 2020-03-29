b87156c6430343918f193592360aa69aa1389dad
Garen Sahagian <g.s.sahagian@gmail.com>
2020-03-29 15:20:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,10 @@
 def reverse_each_word(string)
   reverse_array = string.split()
+  reversed_words =""
   pp reverse_array
   puts reverse_array[0].reverse
   reverse_array.each do |word| 
-    word.reverse
+    reversed_word.concat(word.reverse)
   end
   reverse_array.to_s
   reverse_array
